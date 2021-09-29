c3108f27238903d4bbb0b91a0b43b591bcaeec24
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2021-09-30 10:55:27 +1300

    Remove raycaster

--- dist/ControlRig.js
+++ dist/ControlRig.js
@@ -2,11 +2,9 @@ import { memo, useCallback, useEffect } from 'react';
 import { useThree } from '@react-three/fiber';
 import gesturestream from './gesturestream';
 import flydDomEvents from 'flyd-dom-events';
-import { Raycaster, Vector2 } from 'three';
 // Handle interaction events on a DOM object
 const ControlRig = memo(({ onGesture, domTarget }) => {
     const target = useThree(useCallback(s => domTarget || s.gl.domElement, [domTarget]));
-    const camera = useThree(s => s.camera);
     // PreventDefault on certain events
     useEffect(() => {
         if (!target)
@@ -24,17 +22,7 @@ const ControlRig = memo(({ onGesture, domTarget }) => {
     useEffect(() => {
         if (!target)
             return;
-        const raycaster = new Raycaster();
-        const coords = new Vector2();
-        const stream = gesturestream(target)
-            .map((e) => {
-            // Update raycaster
-            const { center, targetSize } = e.gesture;
-            coords.set((center[0] / targetSize[0]) * 2 - 1, -(center[1] / targetSize[1]) * 2 + 1);
-            raycaster.setFromCamera(coords, camera);
-            return { raycaster, ...e };
-        })
-            .map(onGesture);
+        const stream = gesturestream(target).map(onGesture);
         return () => {
             stream.end(true);
         };
